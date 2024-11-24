185e09aa89f0ef71538eba99dde5490fecf0d58e
mubinawaheed <mubinawaheed1@gmail.com>
2024-11-24 14:03:13 +0500

    Leetcode problem - 2

--- remove-duplicates.py
+++ remove-duplicates.py
@@ -1,5 +1,3 @@
-# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
-
 class Solution(object):
     def removeDuplicates(self, nums):
         """
@@ -8,8 +6,10 @@ class Solution(object):
         """
         i=0
         while True:
-            if(i==len(nums)-1):
-                break
-            while nums[i]==nums[i+1]:
+            while  i<len(nums)-1 and nums[i]==nums[i+1]:
                 del nums[i]
             i+=1
+            if(i>=len(nums)-1):
+                break
+                
+            
