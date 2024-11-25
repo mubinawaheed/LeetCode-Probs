e7f911d2e9341ed5c7aaf4557697fe3b2e240f56
mubinawaheed <mubinawaheed1@gmail.com>
2024-11-25 23:03:02 +0500

    leetcode problem 3

---
+++ remove-element.py
@@ -0,0 +1,14 @@
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        while val in nums:
+            index = nums.index(val)
+            del nums[index]
+
+        return len(nums)
