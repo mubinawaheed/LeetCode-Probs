32efda495730a8c844247cf0a5f634dca4cf1be6
mubinawaheed <mubinawaheed1@gmail.com>
2024-11-27 00:17:25 +0500

    leetcode problem 6

---
+++ single-number.py
@@ -0,0 +1,37 @@
+#  Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space. 
+
+class Solution(object):
+    
+    #Un optimized solution
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d={}
+        for i in range(len(nums)):
+            
+            if (d.get(nums[i]) is not None):
+                
+                d[nums[i]]+=1
+            else:
+                
+                d[nums[i]]=1
+                
+        d = next(((k, v) for k, v in d.items() if v == 1), None)
+
+        return d[0]
+
+    def singleNumberOptimized(self, nums): # using bitwise exclusive XOR. 2 XOR 2 = 0 but 2 XOR 0 =2
+        res=0
+        for i in nums:
+            res = i ^ res
+        return res
+    
+nums=[1,1,2,4,2]
+res=0
+for i in nums:
+    res = i ^ res
+print(res)
