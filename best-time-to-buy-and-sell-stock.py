0f710ee062bea4bf7ff6e25b4e3dc0775db14576
mubinawaheed <mubinawaheed1@gmail.com>
2024-12-22 01:15:20 +0500

    leetcode problem - 48

--- best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock.py
@@ -3,11 +3,18 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        lowestPrice = min(prices)
-        dayToBuy = prices.index(lowestPrice)
-        if(dayToBuy == len(prices)-1):
-            return 0
-        else:
-            highestPrice = max(prices[dayToBuy:])
-            dayToSell = prices[dayToBuy:].index(highestPrice)
-            return highestPrice-lowestPrice
+        maxP=0
+        l,r=0,1
+        while r<len(prices):
+            if(prices[l]<prices[r]):
+                profit = prices[r]-prices[l]
+                maxP= max(profit, maxP)
+
+            else:
+                l=r
+            r+=1
+        return maxP
+    
+s=Solution()
+p=s.maxProfit([7,1,5,0,6,4])
+print(p)
