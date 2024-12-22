3a0473c67380d4f3ff33e649a93350752015667e
mubinawaheed <mubinawaheed1@gmail.com>
2024-12-22 18:45:52 +0500

    leetcode problem - 54.py

---
+++ Move zeros in-place.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count=0
+        i=0
+        while i<len(nums):
+            if nums[i] == 0:
+                del nums[i]
+                count+=1
+            else:
+                i+=1
+        for i in range(count):
+            nums.append(0)
+        
