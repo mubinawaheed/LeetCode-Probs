48b1938e825af7ff5dcbe815be545c89e9203f20
mubinawaheed <mubinawaheed1@gmail.com>
2025-01-18 19:34:38 +0500

    leetcode problem - 108

--- Count Pairs Of Similar Strings.py
+++ Count Pairs Of Similar Strings.py
@@ -1,12 +1,24 @@
-from typing import List
-
+from typing import Counter, List
 
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        u=0
-        for i in range(len(words)-1):
-            for j in range(i+1, len(words)):
-                if (set(words[i]) == set(words[j])):
-                    u+=1
-        return u
-        
+        # u=0
+        # for i in range(len(words)-1):
+        #     for j in range(i+1, len(words)):
+        #         if (set(words[i]) == set(words[j])):
+        #             u+=1
+        # # return u
+
+        s={}
+        for i in words:
+            c=Counter(i)
+            p="".join(sorted(c.keys()))
+            if p in s:
+                s[p]+=1
+            else:
+                s[p]=1
+        count=0
+        for v in s.values():
+            o=v*(v-1)//2
+            count+=o
+        return count
