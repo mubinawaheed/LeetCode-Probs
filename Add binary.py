d854975f8d2504a632285945a5e89d6b31ade73a
mubinawaheed <mubinawaheed1@gmail.com>
2025-01-19 19:43:23 +0500

    leetcode problem - 110

--- Add binary.py
+++ Add binary.py
@@ -1,30 +1,19 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if (len(a) != len(b)):
-            d=abs(len(a) - len(b))
-            if len(a)>len(b):
-                i='0'*d
-                i+=b
-                b=i
+        if len(a) != len(b):
+            d = abs(len(a) - len(b))
+            if len(a) > len(b):
+                b = "0" * d + b
             else:
-                i='0'*d
-                i+=a
-                a=i
-        c=0
-        print(a,b)
-        res=''
-        for i in range(len(a)-1,-1,-1):
-            a1=int(a[i],2)
-            b2=int(b[i],2)
-            total = a1+b2+c
-            res = str(total%2) + res
-            c=total//2
+                a = "0" * d + a
+        c = 0
+        res = ""
+        for i in range(len(a) - 1, -1, -1):
+            a1 = int(a[i], 2)
+            b2 = int(b[i], 2)
+            total = a1 + b2 + c
+            res = str(total % 2) + res
+            c = total // 2
         if c:
-            res=str(c)+res
+            res = str(c) + res
         return res
-
-
-
-
-
-        
