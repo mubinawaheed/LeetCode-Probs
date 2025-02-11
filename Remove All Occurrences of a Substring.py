17bec202ab0e84326551e1b7b02b273c9ddb3287
mubinawaheed <mubinawaheed1@gmail.com>
2025-02-12 00:06:32 +0500

    leetcode problem - 146

--- Remove All Occurrences of a Substring.py
+++ Remove All Occurrences of a Substring.py
@@ -4,6 +4,19 @@ class Solution:
             a=s.find(part)
             s=s[0:a] + s[a+len(part):]    
         return s
+
+
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack=[]
+        
+        for i in s:
+            stack.append(i)
+            if (len(stack)>=len(part) and ''.join(stack[-len(part):])==part):
+                for j in range(len(part)):
+                    stack.pop()
+        return "".join(stack)
+                    
     
 s=Solution()
-print(s.removeOccurrences("gjzgbpggjzgbpgsvpwdk", "gjzgbpg"))
+print(s.removeOccurrences("daabcbaabcbc", "abc"))
