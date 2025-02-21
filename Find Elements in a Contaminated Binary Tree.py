65f771141203d83cc61e5297c3cc9597f09dd146
mubinawaheed <mubinawaheed1@gmail.com>
2025-02-22 00:59:47 +0500

    leetcode problem - 151

--- Find Elements in a Contaminated Binary Tree.py
+++ Find Elements in a Contaminated Binary Tree.py
@@ -1,9 +1,13 @@
 # Definition for a binary tree node.
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+        
 class FindElements:
 
     def __init__(self, root: Optional[TreeNode]):
