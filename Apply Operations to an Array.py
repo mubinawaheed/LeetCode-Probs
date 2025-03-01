a42b6c4ef265d69d2f43bfd98343d81043a51b73
mubinawaheed <mubinawaheed1@gmail.com>
2025-03-01 19:05:44 +0500

    leetcode problem - 159

--- Apply Operations to an Array.py
+++ Apply Operations to an Array.py
@@ -17,3 +17,19 @@ class Solution:
             k=nums.index(0)
             del nums[k]
         return nums + [0]*count_of_zeros
+    
+    def applyOperations(self, nums: List[int]) -> List[int]: # better approach
+        
+        for i in range(len(nums)-1):   
+            if (nums[i] == nums[i+1]) and (nums[i] !=0):
+                nums[i]=nums[i]*2
+                nums[i+1]=0
+
+        l=0
+        for j in range(len(nums)):
+            if nums[j]:
+                nums[j], nums[l] = nums[l],nums[j]
+                l+=1
+
+        return nums 
+    
