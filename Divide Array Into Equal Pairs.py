19e187a21adfa8829094a842a03fbba071b83e10
mubinawaheed <mubinawaheed1@gmail.com>
2025-03-18 00:35:16 +0500

    leetcode problem - 174

---
+++ Divide Array Into Equal Pairs.py
@@ -0,0 +1,12 @@
+from typing import Counter, List
+
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+
+        c=Counter(nums)
+
+        for i in c.values():
+            if i%2 != 0:
+                return False
+        return True
