41df6da0cfcf6341117cf4a9acda7a533907c50e
mubinawaheed <mubinawaheed1@gmail.com>
2025-04-03 00:04:42 +0500

    leetcode problem - 187

---
+++ Maximum Value of an Ordered Triplet I.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        res=0
+        n=len(nums)
+        left=nums[0]
+        for j in range(1, n):
+            if nums[j] > left:
+                left=nums[j]
+                continue
+            for k in range(j+1, n):
+                res=max(res, (left-nums[j]) * nums[k])
+        return res
