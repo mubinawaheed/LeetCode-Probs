a527a364f238d16ec379e0ce2006c154dbb526db
mubinawaheed <mubinawaheed1@gmail.com>
2025-04-14 23:38:17 +0500

    leetcode problem - 195

---
+++ Count Good Triplets.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        n = len(arr)
+        cnt = 0
+        for i in range(n-2):
+            for j in range(i+1, n-1):
+                if abs(arr[i]-arr[j])>a:
+                    continue
+                for k in range(j + 1, n):
+                    if (
+                        abs(arr[i] - arr[j]) <= a
+                        and abs(arr[j] - arr[k]) <= b
+                        and abs(arr[i] - arr[k]) <= c
+                    ):
+                        cnt += 1
+        return cnt
