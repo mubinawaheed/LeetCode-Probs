3cfa23692bb2321ceda54ed6256f9c6c242f19a9
mubinawaheed <mubinawaheed1@gmail.com>
2025-04-27 11:54:31 +0500

    leetcode problem - 200

---
+++ Count Subarrays of Length Three With a Condition.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        res=0
+        for i in range(len(nums)-2):
+            if nums[i+1]%2 ==0 and (nums[i] + nums[i+2] == nums[i+1]//2):
+                res+=1
+        return res
